949585ef24e9925de84d436018b54e4e4af7f6e1
Matt Lane <lane.matthew@gmail.com>
2017-07-20 16:38:49 -0700

    adding modal

--- public/client.js
+++ public/client.js
@@ -2,4 +2,14 @@ $(function() {
 
   io.connect();
 
+  if (!localStorage.getItem('username')) {
+    var $modal = $("#modal");
+    $modal.modal({ keyboard: false });
+    $modal.find('form').on('submit', function(e) {
+      e.preventDefault();
+      localStorage.setItem('username', $modal.find('input').val())
+      $modal.modal('hide');
+    });
+  }
+
 });
