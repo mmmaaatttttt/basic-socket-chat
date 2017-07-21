ed6f3474f27e68f402f5ba842cc1ae02a97ac525
Matt Lane <lane.matthew@gmail.com>
2017-07-20 17:21:48 -0700

    adding socket functionality for users and messages

--- app.js
+++ app.js
@@ -7,7 +7,22 @@ app.use(express.static(__dirname + "/public"));
 app.use('/scripts', express.static(__dirname + "/node_modules"));
 app.set('view engine', 'pug');
 
-io.on('connection', () => console.log("NEW USER!"))
+io.on('connection', socket => {
+  let username = ''
+
+  socket.on('new user from client', data => {
+    username = data;
+    io.emit('new user from server', data);
+  });
+
+  socket.on('disconnect', (data, foo) => {
+    io.emit('disconnected user from server', username);
+  });
+
+  socket.on('new message from client', data => {
+    io.emit('new message from server', data);
+  });
+});
 
 app.get('/', (req, res) => res.render('index'));
 
--- public/client.js
+++ public/client.js
@@ -1,15 +1,50 @@
 $(function() {
 
-  io.connect();
+  var socket = io();
+  var $messages = $("#messages");
+  var $form = $("#new-message-form");
+  var $textarea = $("#new-message");
 
   if (!localStorage.getItem('username')) {
     var $modal = $("#modal");
     $modal.modal({ keyboard: false });
     $modal.find('form').on('submit', function(e) {
       e.preventDefault();
-      localStorage.setItem('username', $modal.find('input').val())
+      var username = $modal.find('input').val();
+      localStorage.setItem('username', username)
       $modal.modal('hide');
+      socket.emit('new user from client', username);
     });
+  } else {
+    var username = localStorage.getItem('username');
+    socket.emit('new user from client', username);
   }
+
+  $form.on('submit', function(e) {
+    e.preventDefault();
+    var newMessage = $textarea.val();
+    socket.emit('new message from client', newMessage);
+    $textarea.val('');
+  });
+
+  socket.on('new user from server', function(data) {
+    $messages.append($("<li>", {
+      class: 'user-info',
+      text: data + " has joined the chat!"
+    }));
+  });
+
+  socket.on('disconnected user from server', function(data) {
+    $messages.append($("<li>", {
+      class: 'user-info',
+      text: data + " has left the chat :("
+    }));
+  });
+
+  socket.on('new message from server', function(data) {
+    $messages.append($("<li>", {
+      text: username + ": " + data
+    }));
+  });
 
 });
